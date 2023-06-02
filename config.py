e29dde743434d104f5b6d817bb1142dfcaadb267
aakash21214 <93417145+aakash21214@users.noreply.github.com>
2023-06-02 13:38:56 +0000

    config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "12916125")
+    API_HASH  = os.environ.get("API_HASH", "dfebf9cc52b859771cf8a1d447e751a5")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5923462655:AAGABmVO38Umc3c764Doh1u3mdpW5yv773w") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
@@ -37,12 +37,12 @@ class Config(object):
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1651746145').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001913986602") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
 
     # wes response configuration     
-    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
 
 
 
